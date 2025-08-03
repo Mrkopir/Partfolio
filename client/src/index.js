05ad03bdb11c84921732ea7a7747d15390ff668a
Disp@tcheer <mrkopir@gmail.com>
2025-08-03 22:48:02 +0300

    version 1.1.6

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 import "./reuseble/enter.motion/enter.motion.style/enter.motion.css"
 import "./reuseble/nav/nav.css"
--- client/src/pages/contact.page/contact.page.form.jsx
+++ client/src/pages/contact.page/contact.page.form.jsx
@@ -17,7 +17,7 @@ export default function ContactPageForm () {
     const onHandleSubmit = async (e) => {
         e.preventDefault()
         if (!data.name || !data.email || !data.phone || !data.question) return setError(true)
-        await ContactSender(data).then(() => window.location.reload())
+        console.log(await ContactSender(data).then(() => window.location.reload()))
     }
 
     return(
